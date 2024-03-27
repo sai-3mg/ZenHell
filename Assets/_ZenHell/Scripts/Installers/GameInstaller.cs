80bb6e5a825317e17dfbfd266bfa4da44286cdf2
SaikrishnaCzar <sai@3mg.games>
2024-03-28 03:03:12 +0530

    GameInstaller and Basic placeholder managers

---
+++ Assets/_ZenHell/Scripts/Installers/GameInstaller.cs
@@ -0,0 +1,13 @@
+using Zenject;
+
+namespace ZenHell.Installers
+{
+    public class GameInstaller : MonoInstaller<GameInstaller>
+    {
+        public override void InstallBindings()
+        {
+            // Dependency bindings will be added here in the future.
+        }
+    }
+
+}
